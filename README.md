82d1b07beccac90952dad648cbbb8279f497eec2
Jawn <jawnchuks@gmail.com>
2025-02-15 14:59:03 +0100

    modified mobile logo and preview

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Next.js + EmailOctopus wait list page
+# Gadahere waitlist page
 
 ![Page preview](./preview.png)
 
@@ -6,7 +6,6 @@
 
 A ready-to-use Next.js wait list page that helps you capturing potential customer emails using EmailOctopus service.
 
-- Designed using Figma
 - Built using powerful technologies: Next.js, Typescript and Tailwind CSS.
 - Mobile responsive
 
@@ -20,7 +19,7 @@ A ready-to-use Next.js wait list page that helps you capturing potential custome
 
 - Install dependencies
   ```bash
-  pnpm install
+  yarn install
   ```
 
 
@@ -28,10 +27,5 @@ A ready-to-use Next.js wait list page that helps you capturing potential custome
 
 Once you have successfully deployed the project, ensure to set the environment variables as outlined in the `.env.example` file on your hosting platform.
 
-If you encounter any issues during this process, please don't hesitate to contact me at: soufiane-elc@outlook.com.
-
-## Support
-
-If you appreciate my work, you can show your support by [buying me a coffee](https://www.buymeacoffee.com/soufianeelc). Your contribution helps keep the momentum going and fuels the creation of more content.
 
 Thank you for your generosity! ☕
--- app/page.tsx
+++ app/page.tsx
@@ -8,21 +8,38 @@ export default function Home() {
       <Toaster />
 
       <section className="w-screen h-dvh grid grid-cols-1 md:grid-cols-2 gap-6">
-        <div className="md:h-full h-80 bg-[#FCD0A1] relative overflow-hidden">
+        <div className="md:h-full h-80 bg-[#F4aa06]/80 relative overflow-hidden">
           <Image
             src="/mobile-app.png"
             alt="Mobile App Screenshots"
             fill
-            className="object-contain mt-8 md:mt-24 px-14 object-bottom"
+            className="object-contain px-14 object-center"
           />
         </div>
 
-        <main className="flex flex-col gap-8 mt-8 justify-center px-6 pb-10">
-          <h1 className="font-semibold tracking-tight text-zinc-900 text-3xl leading-tight md:text-4xl max-w-lg">
-            Join the conversation: Secure your spot on our Chat App!
+        <main className="flex flex-col gap-8 mt-8 md:mt-20 justify-start px-6 pb-10">
+          <div className="h-20 relative overflow-hidden">
+            <Image
+              src="/logo.png"
+              alt="Mobile App Screenshots"
+              fill
+              className="object-contain px-14 object-bottom"
+            />
+          </div>
+          <h1 className="font-semibold tracking-tight text-zinc-900 text-3xl leading-tight md:text-4xl max-w-lg text-center md:text-left">
+            Gadahere is Coming Soon – Be the First to Join!
           </h1>
           <p className="text-gray-500">
-            Join the waitlist to be notified when our app is available!
+            Imagine a world where saving and sharing products is effortless,
+            discovering the best deals is seamless, and connecting with vendors
+            happens in just a tap. That’s what Gadahere is all about! Our app is
+            designed to help you explore products with ease, save your
+            favorites, and interact with vendors—all in one place.
+          </p>
+          <p className="text-gray-500">
+            {" "}
+            Be among the first to experience the future of effortless shopping
+            and sharing. Click below to join our community and stay updated! 🚀
           </p>
 
           <EmailForm />
